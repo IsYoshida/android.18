2c0b9e6c3b220d296eace654cc9ff794b7057bf7
IsYoshida <nosyalk@gmail.com>
2021-05-23 14:49:09 -0300

    First commit

---
+++ src/vipbot.js
@@ -0,0 +1,19 @@
+const vipbot = (prefix) => {
+    return ` *ANDROID No.18* - _BOT_
+★・・・★・・・ - ・・・★・・・★
+
+🎉 *Support exclusivo:* _*Indiponínel*_
+
+🎉 *Adicionar comando:* _*Insdiponível*_
+
+🎉 *Criar número:* _*Insdiponível*_
+
+🎉 *Código original:* _*Insdiponível*_
+
+🎉 *Outros comandos:* _*Indisponível*_
+
+ℹ️ Mais informações ℹ️
+
+_${prefix}criador_`
+}
+exports.vipbot = vipbot
